1e514e54f96630c8cf205cf5cc0d98e7c93d65bb
Jesse Vincent <jesse@bestpractical.com>
2012-07-24 15:36:12 -0700

    jsbeautify

--- app/scripts/rei.js
+++ app/scripts/rei.js
@@ -1,33 +1,34 @@
-define( 
- ['jQuery'], function(jquery) { 
-    
-     var jQuery = jquery;
-     return { responsePlugins: [],
-        
+define(
+['jQuery'], function(jquery) {
+    var jQuery = jquery;
 
-initializePlugins: function(pluginNames) {
-    var that = this;
+    return {
+        responsePlugins: [],
+        initializePlugins: function(pluginNames) {
+            var that = this;
 
+            jQuery.each(pluginNames, function(index, name) {
+                requirejs([name],
 
-jQuery.each(pluginNames, function(index,name) {
-requirejs([name],
-function   (plugin) {
-    that.responsePlugins.push(plugin);
+                function(plugin) {
+                    that.responsePlugins.push(plugin);
 
+                });
+
+            });
+        },
+
+        handleQuery: function(input) {
+            var that = this;
+            var responses = [];
+
+            jQuery.each(that.responsePlugins, function(index, plugin) {
+                responses[plugin.name] = plugin.reply({
+                    rawInput: input,
+                    jQuery: jQuery
+                });
+            });
+            return responses;
+        }
+    }
 });
-
-});
-},
-
-
-
-
-    handleQuery: function(input) {
-        var that = this;
-    var responses = [];
-
-jQuery.each(that.responsePlugins, function(index,plugin) {
-    responses[plugin.name] = plugin.reply({ rawInput: input, jQuery: jQuery } );
-});
-    return responses;
-}}});

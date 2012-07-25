4e59fd0ba2e09a35741da22b9b548bdccdee784d
Jesse Vincent <jesse@bestpractical.com>
2012-07-25 16:32:33 -0700

    make the webui hurt a little less

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,13 +8,20 @@ requirejs(['./rei'], function(rei) {
 
 function handleQuery() {
     var cmd = jQuery("#q").val();
-    $("#conversation").append('Person: <div class="user">'+cmd+'</div>');
+    $("#conversation").append('<div class="user"><span class="yourname">You:</span> <span class="msg">'+cmd+'</span></div>');
     var response = Rei.handleQuery(cmd);
-    $("#conversation").append('Bot: <div class="rei">'+JSON.stringify(response)+'</div>');
+    $("#conversation").append('<div class="rei"><span class="reiname">Rei:</span> <span class="msg">'+JSON.stringify(response)+'<span></div>');
         
 } 
 
 jQuery("#q").on("blur", function() {
     handleQuery();
+    jQuery("#q").val('').focus();
 
+}).on("keyup", function(e) {
+      if(e.which == 13) {
+           e.preventDefault();
+    handleQuery();
+    jQuery("#q").val('').focus();
+             }
 });

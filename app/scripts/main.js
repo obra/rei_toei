a8b01f9267f63a1b7ccd8d8cfe64972bd107c303
Jesse Vincent <jesse@bestpractical.com>
2012-07-25 17:28:47 -0700

    She says hi

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,20 +8,33 @@ requirejs(['./rei'], function(rei) {
 
 function handleQuery() {
     var cmd = jQuery("#q").val();
+    if (cmd === '')
+        return;
     $("#conversation").append('<div class="user"><span class="yourname">You:</span> <span class="msg">'+cmd+'</span></div>');
     var response = Rei.handleQuery(cmd);
-    $("#conversation").append('<div class="rei"><span class="reiname">Rei:</span> <span class="msg">'+JSON.stringify(response)+'<span></div>');
-        
+    reiSay(JSON.stringify(response));
 } 
 
-jQuery("#q").on("blur", function() {
+function reiSay(string) {
+    $("#conversation").append('<div class="rei"><span class="reiname">Rei:</span> <span class="msg">'+string+'<span></div>');
+
+}
+
+
+jQuery("#submit").on("click", function() {
     handleQuery();
     jQuery("#q").val('').focus();
+});
 
-}).on("keyup", function(e) {
+jQuery("#q").on("keyup", function(e) {
       if(e.which == 13) {
            e.preventDefault();
     handleQuery();
     jQuery("#q").val('').focus();
              }
+});
+
+jQuery().ready(function() {
+    reiSay("Hi! I'm Rei Toei. What's your name?");
+
 });

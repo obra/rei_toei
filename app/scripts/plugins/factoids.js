399304b32dccfa4de40734bf78f34758a12bbed5
Jesse Vincent <jesse@bestpractical.com>
2012-07-26 16:38:47 -0700

    added a way to get a factoid braindump

--- app/scripts/plugins/factoids.js
+++ app/scripts/plugins/factoids.js
@@ -3,30 +3,35 @@ define(['plugins/factoids'], function() {
     return {
         name: 'factoids',
         reply: function(args) {
+            if (args.rawInput.match(/^factoid braindump$/i)) {
+                return [1, Rei.dumpFactoids()];
+            }
             if (matching = args.rawInput.match(/^\s*(.*?)\sis\s(.*)$/i)) {
                 var key = matching[1];
                 var value = matching[2];
                 if (!key.match(/^what$/i)) {
-                 this.putFactoid(key, value);
-                return [1, "Ok. I've made a note of it"];
+                    this.putFactoid(key, value);
+                    return [1, "Ok. I've made a note of it"];
                 }
             }
-                var query = args.rawInput.replace(/\?$/,'').replace(/^What is/i,''); 
-                var result = this.getFactoid(query);
-                if (result !== null) {
-                    return [1, result];
-                } else {
-                    return [0, "Nothing there. Teach me?"];
-                }
+            var query = args.rawInput.replace(/\?$/, '').replace(/^What is/i, '');
+            var result = this.getFactoid(query);
+            if (result !== null) {
+                return [1, result];
+            } else {
+                return [0, "Nothing there. Teach me?"];
+            }
         },
-        putFactoid: function(key,value) {
-                // TODO - check for existing factoid with this name here
-                Rei.putFactoid(key, value);
+        putFactoid: function(key, value) {
+            // TODO - check for existing factoid with this name here
+            Rei.putFactoid(key, value);
         },
         getFactoid: function(key) {
             return Rei.getFactoid(key);
+        },
+        dumpFactoids: function() {
+
         }
-        
+
     }
-}
-);
+});
--- app/scripts/rei.js
+++ app/scripts/rei.js
@@ -12,16 +12,32 @@ define(
         storage: window.localStorage,
 
         getFactoid: function(query) {
-           var key = jQuery.trim(query.toLowerCase()); 
-           console.log("Looking for a factoid called: "+key);
-           var value =  this.storage.getItem("factoid."+key);
-           return value;
+            var key = jQuery.trim(query.toLowerCase());
+            console.log("Looking for a factoid called: " + key);
+            var value = this.storage.getItem("factoid." + key);
+            return value;
         },
         putFactoid: function(key, value) {
-            key = jQuery.trim(key.toLowerCase()); 
-            console.log("Learning that '"+key+"' is '"+value+"'");
-            this.storage.setItem("factoid."+key, value);
+            key = jQuery.trim(key.toLowerCase());
+            console.log("Learning that '" + key + "' is '" + value + "'");
+            this.storage.setItem("factoid." + key, value);
         },
+        dumpFactoids: function() {
+            var data = {};
+        console.log("dumpFactoids");
+            for (i = 0; i <= this.storage.length - 1; i++) {
+                console.log("I is "+i);
+                key = this.storage.key(i);
+                console.log("key is "+key);
+                if ( res = key.match(/^factoid\.(.*)$/)) { 
+                    console.log("Grabbing "+key);
+                    data[res[1]] = this.storage.getItem(key);
+                }
+            }
+            return data;
+        },
+
+
         initializePlugins: function(pluginNames) {
             var that = this;
 

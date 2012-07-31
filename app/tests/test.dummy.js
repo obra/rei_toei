9a2a6856beeb2f9954a0a32ac8aacc62a24b0832
Yoz Grahame <yoz@yoz.com>
2012-07-30 22:45:52 -0700

    Added beginnings of test runner - still need Rei.initialize() to take a callback

---
+++ app/tests/test.dummy.js
@@ -0,0 +1,18 @@
+// At the start of every test file, we want Rei's storage to be blank.
+
+describe('Factoid plugin', function(){
+	before(function() {
+		var should = chai.should();
+
+		Rei.storage = sessionStorage;
+		Rei.storage.clear();
+		Rei.initialize(['./plugins/factoids']);
+	});
+	describe('name memory', function(){
+		it('should learn my name', function(){
+			var response = Rei.handleQuery("my name is Yoz");
+			console.log("Factoid response:", response);
+			response.factoids[0].should.equal(0);
+		});
+	});
+});

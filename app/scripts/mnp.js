789e338b546f8668f004737cb7f2b68b4fc6787d
Schuyler Erle <schuyler@nocat.net>
2012-08-07 17:27:48 -0700

    Port over some code from Lingua::EN::Tagger to extract maximal noun phrases from POS-tagged tokens.

---
+++ app/scripts/mnp.js
@@ -0,0 +1,64 @@
+/* compute maximal noun phrases from tagged text
+   the relevant parts are from Lingua-EN-Tagger/Tagger.pm */
+ 
+tags = {
+    NUM  : 'CD',
+    GER  : 'VBG',
+    ADJ  : 'JJ[RS]*',
+    PART : 'VBN',
+    NN   : 'NN[SP]*',
+    NNP  : 'NNP',
+    PREP : 'IN',
+    DET  : 'DET',
+    PAREN: '[LR]RB',
+    QUOT : 'PPR',
+    SEN  : 'PP'
+};
+
+max_noun_regex = "\
+    # optional number, gerund - adjective -participle   \
+    (?:$NUM)?(?:$GER|$ADJ|$PART)*                       \
+    # Followed by one or more nouns                     \
+    (?:$NN)+                                            \
+        (?:                                             \
+        # Optional preposition, determinant, cardinal   \
+        (?:$PREP)*(?:$DET)?(?:$NUM)?                    \
+        # Optional gerund or adjective or participle    \
+        (?:$GER|$ADJ|$PART)*                            \
+        # one or more nouns                             \
+        (?:$NN)+                                        \
+    )*                                                  \
+";
+
+max_noun_regex = max_noun_regex.replace(/\$([A-Z]+)/g,
+                    function(_, arg) { return "\\d+:" + tags[arg] } );
+max_noun_regex = max_noun_regex.replace(/\s+(?:#.*?\n)/g, " ");
+max_noun_regex = new RegExp(max_noun_regex, "g");
+
+function get_noun_phrases(tokens) {
+    structure = [];
+    jQuery.each(tokens, function (i, pair) {
+        // pair = [token, pos]
+        structure.push(i + ":" + pair[1]);
+    });
+    structure = structure.join(" ");
+    matches = structure.match(max_noun_regex);
+    mnps = [];
+    jQuery.each(matches, function(i, match) {
+        mnp = [];
+        jQuery.each(match, function(j, pos) {
+            pos = pos.split(":");
+            mnp.push(tokens[pos[0]]);
+        });
+        mnps.push(mnp);
+    });
+    return mnps;
+};
+
+var input = process.argv[2];
+var Pos = require("./pos");
+var lex = new Pos.Lexer().lex(input);
+var tokens = new Pos.Tagger().tag(lex);
+
+console.log(tokens);
+

64a7dfec0161b572cfb769cac46feea914d80758
Jesse Vincent <jesse@bestpractical.com>
2012-08-07 17:33:29 -0700

    node commandline wrappers for debugging mnp.js
    You'll also want to "npm install requirejs"

--- app/scripts/mnp.js
+++ app/scripts/mnp.js
@@ -1,3 +1,23 @@
+
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Use node's special variable __dirname to
+    //get the directory containing this file.
+    //Useful if building a library that will
+    //be used in node but does not require the
+    //use of node outside
+    baseUrl: __dirname,
+
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+
+
+
 /* compute maximal noun phrases from tagged text
    the relevant parts are from Lingua-EN-Tagger/Tagger.pm */
  
@@ -56,9 +76,16 @@ function get_noun_phrases(tokens) {
 };
 
 var input = process.argv[2];
-var Pos = require("./pos");
+
+
+
+
+
+requirejs(["./pos"], function (Pos) {
+    
 var lex = new Pos.Lexer().lex(input);
 var tokens = new Pos.Tagger().tag(lex);
 
 console.log(tokens);
+});
 
